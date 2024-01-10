63ce0e956de972bcd72df35e18554dd0a72f29fa
Mehmet Tekin ALTUN <tmehmet48.12@gmail.com>
2024-01-10 17:38:55 +0300

    Initial commit

---
+++ eureka-server/src/test/java/com/mta/stockmanagement/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mta.stockmanagement.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
